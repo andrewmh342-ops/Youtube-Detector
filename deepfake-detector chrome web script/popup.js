3743bf28f084ec5546b0ab24987837a2dafb5ceb
cookidas <inteobseb@gmail.com>
2026-01-06 23:03:40 +0900

    Delete deepfake-detector chrome web script directory

--- deepfake-detector chrome web script/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('scanBtn').addEventListener('click', async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  if (tab) {
-    // content.js에 탐지 시작 명령 전송
-    chrome.tabs.sendMessage(tab.id, { action: "START_DETECTION_FLOW" });
-    window.close();
-  }
-});
